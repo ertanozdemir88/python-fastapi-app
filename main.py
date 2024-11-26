701aa7808c1632c3380a698a4598e7fb1c10aa3c
ertanozdemir88 <ertanozdemir88@gmail.com>
2024-11-26 22:13:52 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/greet/{name}")
+def greet_name(name: str):
+    return {"message": f"Hello, {name}!"}
